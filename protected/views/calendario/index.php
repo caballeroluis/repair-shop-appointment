0a845f9ebbe6efa1ecc1fd70b0e4e3bd1890f035
Luis Caballero <111797757+caballeroluis@users.noreply.github.com>
2016-05-11 22:05:34 +0200

    controllerCalendario: creo CalendarioController. meto linea jquery

---
+++ protected/views/calendario/index.php
@@ -0,0 +1,17 @@
+<?php
+
+Yii::app()->clientScript->registerCoreScript('jquery');
+
+
+/* @var $this CalendarioController */
+
+$this->breadcrumbs=array(
+	'Calendario',
+);
+?>
+<h1><?php echo $this->id . '/' . $this->action->id; ?></h1>
+
+<p>
+	You may change the content of this page by modifying
+	the file <tt><?php echo __FILE__; ?></tt>.
+</p>
