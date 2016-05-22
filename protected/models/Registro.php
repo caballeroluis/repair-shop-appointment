08de45bceef0d1bd0e7174be990bce1624d0efc7
Luis Caballero <111797757+caballeroluis@users.noreply.github.com>
2016-05-22 12:54:39 +0200

    scripRegistro: resuelto pequeno conflicto en modelo Registro

--- protected/models/Registro.php
+++ protected/models/Registro.php
@@ -1,141 +1,69 @@
 <?php
 
-/**
- * This is the model class for table "cliente".
- *
- * The followings are the available columns in table 'cliente':
- * @property integer $id
- * @property string $nombre
- * @property string $apellido1
- * @property string $apellido2
- * @property integer $telefono
- * @property integer $codigo_postal
- * @property string $email
- * @property string $fecha_creacion
- * @property string $fecha_modifiacion
- * @property string $password
- * @property integer $vivo
- * @property string $imagen
- * @property string $codigo_activacion
- * @property integer $activado
- * @property string $username
- *
- * The followings are the available model relations:
- * @property Cita[] $citas
- */
-class Registro extends CActiveRecord
-{
-	/**
-	 * @return string the associated database table name
-	 */
-	public function tableName()
-	{
-		return 'cliente';
-	}
+class Registro extends CFormModel {
 
-	/**
-	 * @return array validation rules for model attributes.
-	 */
-	public function rules()
-	{
-		// NOTE: you should only define rules for those attributes that
-		// will receive user inputs.
-		return array(
-			array('nombre, codigo_postal, email, fecha_creacion, password, codigo_activacion, username', 'required'),
-			array('telefono, codigo_postal, vivo, activado', 'numerical', 'integerOnly'=>true),
-			array('nombre, apellido1, apellido2', 'length', 'max'=>45),
-			array('email, username', 'length', 'max'=>128),
-			array('password, imagen, codigo_activacion', 'length', 'max'=>255),
-			array('fecha_modifiacion', 'safe'),
-			// The following rule is used by search().
-			// @todo Please remove those attributes that should not be searched.
-			array('id, nombre, apellido1, apellido2, telefono, codigo_postal, email, fecha_creacion, fecha_modifiacion, password, vivo, imagen, codigo_activacion, activado, username', 'safe', 'on'=>'search'),
-		);
-	}
+  public $username;
+  public $password;
+  public $repetir_password;
+  public $nombre;
+  public $email;
+  public $codigo_postal;
+  public $apellido1;
+  public $apellido2;
+  public $telefono;
+  public $vivo;
+  public $activado;
+  public $imagen;
+  public $codigo_activacion;
+  public $fecha_creacion;
+  public $fecha_modificacion;
+  public $terminos;
 
-	/**
-	 * @return array relational rules.
-	 */
-	public function relations()
-	{
-		// NOTE: you may need to adjust the relation name and the related
-		// class name for the relations automatically generated below.
-		return array(
-			'citas' => array(self::HAS_MANY, 'Cita', 'cliente_id'),
-		);
-	}
+  public function rules() {
 
-	/**
-	 * @return array customized attribute labels (name=>label)
-	 */
-	public function attributeLabels()
-	{
-		return array(
-			'id' => 'ID',
-			'nombre' => 'Nombre',
-			'apellido1' => 'Apellido1',
-			'apellido2' => 'Apellido2',
-			'telefono' => 'Telefono',
-			'codigo_postal' => 'Codigo Postal',
-			'email' => 'Email',
-			'fecha_creacion' => 'Fecha Creacion',
-			'fecha_modifiacion' => 'Fecha Modifiacion',
-			'password' => 'Password',
-			'vivo' => 'Vivo',
-			'imagen' => 'Imagen',
-			'codigo_activacion' => 'Codigo Activacion',
-			'activado' => 'Activado',
-			'username' => 'Username',
-		);
-	}
+    return array(
+        array('nombre, codigo_postal, email, fecha_creacion, password, codigo_activacion, username', 'required', 'message' => 'Este campo es obligatorio'),
+        array('telefono, codigo_postal, vivo, activado', 'numerical', 'integerOnly' => true),
+        array('nombre, apellido1, apellido2', 'length', 'max' => 45, 'tooLong' => 'Máximo 45 caracteres'),
+        array('email, username', 'length', 'max' => 128, 'tooLong' => 'Máximo 128 caracteres'),
+        array('password, imagen, codigo_activacion', 'length', 'max' => 255, 'tooLong' => 'Máximo 255 caracteres'),
+        array('fecha_modificacion', 'safe'),
+        // The following rule is used by search().
+        // @todo Please remove those attributes that should not be searched.
+        array('id, nombre, apellido1, apellido2, telefono, codigo_postal, email, fecha_creacion, fecha_modificacion, password, vivo, imagen, codigo_activacion, activado, username', 'safe', 'on' => 'search'),
+        array(
+            'nombre',
+            'match',
+            'pattern' => '/^[a-záéíóúñàèìòùäëïöüç\s]+$/i',
+            'message' => 'El tipo de datos introducido es incorrecto'
+        ),
+        array(
+            'email',
+            'email',
+            'message' => 'El formato de email introducido no es correcto',
+        ),
+//                    array(
+//                      'password',
+//                      'match',
+//                      'pattern' => '/^.*(?=.{8,16})(?=.*[a-zA-Z])(?=.*\d)(?=.*[!#$%&? "]).*$/',
+//                      'message' => 'Obligatorio legras y números. Símbolos: !#$%&? "',
+//                      'min' => 8,
+//                      'tooShort' => 'Mímimo 8 caracteres',
+//                      'max' => 16,
+//                      'tooLong' => 'Máximo 16 caracteres'
+//                    ),
+        array(
+            'repetir_password',
+            'compare',
+            'compareAttribute' => 'password',
+            'message' => 'Las contraseñas no coinciden',
+        ),
+        array(
+            'terminos',
+            'required',
+            'message' => 'Por favor, acepte los términos'
+        ),
+    );
+  }
 
-	/**
-	 * Retrieves a list of models based on the current search/filter conditions.
-	 *
-	 * Typical usecase:
-	 * - Initialize the model fields with values from filter form.
-	 * - Execute this method to get CActiveDataProvider instance which will filter
-	 * models according to data in model fields.
-	 * - Pass data provider to CGridView, CListView or any similar widget.
-	 *
-	 * @return CActiveDataProvider the data provider that can return the models
-	 * based on the search/filter conditions.
-	 */
-	public function search()
-	{
-		// @todo Please modify the following code to remove attributes that should not be searched.
-
-		$criteria=new CDbCriteria;
-
-		$criteria->compare('id',$this->id);
-		$criteria->compare('nombre',$this->nombre,true);
-		$criteria->compare('apellido1',$this->apellido1,true);
-		$criteria->compare('apellido2',$this->apellido2,true);
-		$criteria->compare('telefono',$this->telefono);
-		$criteria->compare('codigo_postal',$this->codigo_postal);
-		$criteria->compare('email',$this->email,true);
-		$criteria->compare('fecha_creacion',$this->fecha_creacion,true);
-		$criteria->compare('fecha_modifiacion',$this->fecha_modifiacion,true);
-		$criteria->compare('password',$this->password,true);
-		$criteria->compare('vivo',$this->vivo);
-		$criteria->compare('imagen',$this->imagen,true);
-		$criteria->compare('codigo_activacion',$this->codigo_activacion,true);
-		$criteria->compare('activado',$this->activado);
-		$criteria->compare('username',$this->username,true);
-
-		return new CActiveDataProvider($this, array(
-			'criteria'=>$criteria,
-		));
-	}
-
-	/**
-	 * Returns the static model of the specified AR class.
-	 * Please note that you should have this exact method in all your CActiveRecord descendants!
-	 * @param string $className active record class name.
-	 * @return Registro the static model class
-	 */
-	public static function model($className=__CLASS__)
-	{
-		return parent::model($className);
-	}
 }
