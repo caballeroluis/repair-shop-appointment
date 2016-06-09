0f1166b5fd98bc20819097db27ac9b58ef8ec9e4
Luis Caballero <111797757+caballeroluis@users.noreply.github.com>
2016-06-09 17:19:38 +0200

    master: quito modo produccion y agregadores de logs

--- index.php
+++ index.php
@@ -5,17 +5,11 @@ $yii=dirname(__FILE__).'/framework/yii.php';
 $config=dirname(__FILE__).'/protected/config/main.php';
 
 // remove the following lines when in production mode
-defined('YII_DEBUG') or define('YII_DEBUG',true);
+//defined('YII_DEBUG') or define('YII_DEBUG',true);
 // specify how many levels of call stack should be shown in each log message
-defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
+//defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
 
 require_once($yii);
 Yii::createWebApplication($config)->run();
 
 //hacer: comenta la línea 8 en producción
-
-//if (Yii::app()->user->getName() == 'admin') {
-//  defined('YII_DEBUG') or define('YII_DEBUG',true);
-//} else {
-//  defined('YII_DEBUG') or define('YII_DEBUG',false);
-//}
